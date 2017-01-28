3eb6af5a0828ff0f05073c1041e25ce9c30f27a5
jeniby <jeniby16@gmail.com>
2017-01-29 00:11:44 +0300

    new merge

--- sort/sort.py
+++ sort/sort.py
@@ -1,53 +1,46 @@
 def quick_sort(arr): 
-     if len(arr) <= 1:
-          return arr
-     else:
-          return quick_sort([x for x in arr[1:] if x<arr[0]]) + [arr[0]] + quick_sort([x for x in arr[1:] if x>=arr[0]])
+    if len(arr) <= 1:
+        return arr
+    else:
+        lefthalf = quick_sort([x for x in arr[1:] if x<arr[0]])
+        righthalf = quick_sort([x for x in arr[1:] if x>=arr[0]])
+        return lefthalf + [arr[0]] + righthalf
 
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr)//2
-        left = arr[:mid]   #slice
-        right = arr[mid:]   #slice
+def merge_sort(array):
 
-        merge_sort(left)
-        merge_sort(right)
-
+    def merge(left, right):
+        Res = []
         i = 0
         j = 0
-        x = 0
         while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                arr[x] = left[i]
+            if left[i] <= right[j]:
+                Res.append(left[i])
                 i += 1
             else:
-                arr[x] = right[j]
+                Res.append(right[j])
                 j += 1
-            x += 1
+        Res += left[i:] + right[j:]
+        return Res
 
-        while i < len(left):
-            arr[x] = left[i]
-            i += 1
-            x += 1
-        while j < len(right):
-            arr[x] = right[j]
-            j += 1
-            x += 1
+    if len(array) <= 1:
+        return array
+    else:
+        L = array[:len(array) // 2]
+        R = array[len(array) // 2:]
+    return merge(merge_sort(L), merge_sort(R))
 
 def radix_sort(arr):
     rang = 10
     length = len(str(max(arr)))
-    tmp = arr
     for i in range(length):
         arr_rang = [[] for j in range(rang)]
-        for j in tmp:
+        for j in arr:
             figure = j // 10**i % 10
             arr_rang[figure].append(j)  
-        tmp = []
+        arr = []
         for j in range(rang):
-            tmp = tmp + arr_rang[j]
-    for i in range(len(arr)):
-        arr[i] = tmp[i]
+            arr = arr + arr_rang[j]
+    return arr
 
 def main():
     sort = {'1':quick_sort, '2':merge_sort, '3':radix_sort}
