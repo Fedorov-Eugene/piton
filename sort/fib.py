b31a621fecbeaf3ae7bff59e46251e2b7d70a636
polluxCast0r <alexandervirk@gmail.com>
2017-01-28 22:26:36 +0300

    Formula

--- sort/fib.py
+++ sort/fib.py
@@ -1,3 +1,5 @@
+from math import sqrt, pow, floor
+
 def fib(index):
     a = 1
     b = 1 
@@ -7,8 +9,12 @@ def fib(index):
         a, b = b, b + a
         yield b
 
+def fibonacci(index):
+    for _ in range(index):
+        yield floor((pow((1+sqrt(5))/2, _)-pow((1-sqrt(5))/2, _))/sqrt(5))
+
 def main():
-    for i in fib(5):
+    for i in fibonacci(13):
         print(i)
 
 
