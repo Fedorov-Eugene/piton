217aa33537f8c6c9ad2d61dc4f8fb41a696fc74b
polluxCast0r <alexandervirk@gmail.com>
2017-01-29 23:35:27 +0300

    Regex added and test updated

--- tests.py
+++ tests.py
@@ -2,98 +2,20 @@ import sys
 import mathstats.sort as module
 import datetime
 import random
+from views.console_input import request
+from views.console_view import response
 
-def wrapper(text, symbol):
+def suppose(get, awaits):
 	print(symbol + text + symbol + '\033[0m')
+	if func[option](arr) == sorted(arr):
+		response("="*80 + "\nTest passed\n" + "="*80, 'green')
+	else:
+		response("="*80 + "\nTest failed\n" + "="*80 + "\nYour result: " + str(get) + "\nExpected: " + str(awaits), 'red')
 
+def speedcheck(func, iter, *test_data):
+	start = datetime.datetime.now()
+	for i in range(iter):
+		func(test_data)
+	finish = datetime.datetime.now()
+	response(str(finish-start) + " time elapsed")
 
-func = {"quick_sort": module.quick_sort, "merge_sort": module.merge_sort, "radix_sort": module.radix_sort}
-option = input("Enter function name to test: ")
-print("Let's roll through some Pre-Tests:")
-
-print("\nShould work for empty array")
-arr = []
-if func[option](arr) == sorted(arr):
-	wrapper("="*80 + "\nPre-test passed\n" + "="*80, '\033[92m')
-else:
-	wrapper("="*80 + "\nPre-test failed\n" + "="*80 + "\nYour result: " + str(func[option](arr)) + "\nExpected: " + str(sorted(arr)), '\033[91m')
-
-print("\nShould work for array from one element")
-arr = [1]
-if func[option](arr) == sorted(arr):
-	wrapper("="*80 + "\nPre-test passed\n" + "="*80, '\033[92m')
-else:
-	wrapper("="*80 + "\nPre-test failed\n" + "="*80 + "\nYour result: " + str(func[option](arr)) + "\nExpected: " + str(sorted(arr)), '\033[91m')
-
-print("\nShould work for array from negative values")
-arr = [-1, -5, -6, -8, -10, 0]
-if func[option](arr) == sorted(arr):
-	wrapper("="*80 + "\nPre-test passed\n" + "="*80, '\033[92m')
-else:
-	wrapper("="*80 + "\nPre-test failed\n" + "="*80 + "\nYour result: " + str(func[option](arr)) + "\nExpected: " + str(sorted(arr)), '\033[91m')
-
-
-print("\nShould work for simple arrays")
-arr = [4, 5, 6, 78, 87, 23, 14, 67, 45, 22, 23, 90, 1, 3, 9, 56, 55, 50, 30, 11]
-if func[option](arr) == sorted(arr):
-	wrapper("="*80 + "\nFirst test passed\n" + "="*80, '\033[92m')
-else:
-	wrapper("="*80 + "\nFirst test failed\n" + "="*80 + "\nYour result: " + str(func[option](arr)) + "\nExpected: " + str(sorted(arr)), '\033[91m')
-print("Time taken for 10000 iterations:")
-start = datetime.datetime.now()
-for i in range(10000):
-	func[option](arr)
-finish = datetime.datetime.now()
-print(str(finish-start) + " seconds\n")
-
-print("Should work for duplicated numbers")
-arr = [10] * 20
-if func[option](arr) == sorted(arr):
-	wrapper("="*80 + "\nSecond test passed\n" + "="*80, '\033[92m')
-else:
-	wrapper("="*80 + "\nSecond test failed\n" + "="*80 + "\nYour result: " + str(func[option](arr)) + "\nExpected: " + str(sorted(arr)), '\033[91m')
-print("Time taken for 10000 iterations:")
-start = datetime.datetime.now()
-for i in range(10000):
-	func[option](arr)
-finish = datetime.datetime.now()
-print(str(finish-start) + " seconds\n")
-
-print("Should work for presorted arrays")
-arr = list(range(20))
-if func[option](arr) == sorted(arr):
-	wrapper("="*80 + "\nThird test passed\n" + "="*80, '\033[92m')
-else:
-	wrapper("="*80 + "\nThird test failed\n" + "="*80 + "\nYour result: " + str(func[option](arr)) + "\nExpected: " + str(sorted(arr)), '\033[91m')
-print("Time taken for 10000 iterations:")
-start = datetime.datetime.now()
-for i in range(10000):
-	func[option](arr)
-finish = datetime.datetime.now()
-print(str(finish-start) + " seconds\n")
-
-print("Should work for reversed arrays")
-arr = list(reversed(range(20)))
-if func[option](arr) == sorted(arr):
-	wrapper("="*80 + "\nFourth test passed\n" + "="*80, '\033[92m')
-else:
-	wrapper("="*80 + "\nFourth test failed\n" + "="*80 + "\nYour result: " + str(func[option](arr)) + "\nExpected: " + str(sorted(arr)), '\033[91m')
-print("Time taken for 10000 iterations:")
-start = datetime.datetime.now()
-for i in range(10000):
-	func[option](arr)
-finish = datetime.datetime.now()
-print(str(finish-start) + " seconds\n")
-
-print("Should work for REALLY long arrays")
-arr = random.sample(range(1000000), 1000000)
-if func[option](arr) == sorted(arr):
-	wrapper("="*80 + "\nFivth test passed\n" + "="*80, '\033[92m')
-else:
-	wrapper("="*80 + "\nFivth test failed\n" + "="*80 + "\nYour result: " + str(func[option](arr)) + "\nExpected: " + str(sorted(arr)), '\033[91m')
-print("Time taken for 1 iterations:")
-start = datetime.datetime.now()
-for i in range(1):
-	func[option](arr)
-finish = datetime.datetime.now()
-print(str(finish-start) + " seconds\n")
--- views/console_view.py
+++ views/console_view.py
@@ -4,5 +4,5 @@ class ConsoleWriter(object):
 		super(ConsoleWriter, self).__init__()
 		self.colors = {"red": '\033[91m', "yellow": '\033[93m', "green": '\033[92m', "blue": '\033[94m', "white": '\033[0m'}
 
-	def response(text, flag):
+	def response(text, flag = "white"):
 		print(colors[flag] + text + colors["white"])		
